2bda16962c0ae60987bdac7e4b364670aa59f562
Seregon <109359355+seregonwar@users.noreply.github.com>
2023-06-02 18:19:06 +0200

    Integration of a search bar
     Integration of a search bar through which you can make the AI ​​search for what you are looking for

--- AI.py
+++ AI.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton, QVBoxLayout, QWidget, QLabel, QMenu, QMenuBar, QAction, QFileDialog, QSplitter
 from PyQt5.QtGui import QFont, QColor, QPalette
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 from hugchat import hugchat
 from googlesearch import search
 from google.auth import impersonated_credentials
@@ -23,6 +24,20 @@ class ChatWindow(QMainWindow):
         self.central_widget = QSplitter(self)
         self.setCentralWidget(self.central_widget)
 
+        # Barra di ricerca
+        self.search_input = QLineEdit()
+        self.search_button = QPushButton("Search")
+        self.search_button.clicked.connect(self.handle_search)
+
+        search_layout = QVBoxLayout()
+        search_layout.addWidget(self.search_input)
+        search_layout.addWidget(self.search_button)
+
+        search_widget = QWidget()
+        search_widget.setLayout(search_layout)
+
+        self.central_widget.addWidget(search_widget)
+
         self.chat_layout = QVBoxLayout()
         self.central_widget.setLayout(self.chat_layout)
 
@@ -33,7 +48,7 @@ class ChatWindow(QMainWindow):
         self.central_widget.addWidget(self.history_widget)
         self.central_widget.setCollapsible(0, False)
         self.central_widget.setCollapsible(1, True)
-        self.central_widget.setSizes([600, 200])
+        self.central_widget.setSizes([200, 600])
 
         self.chat_layout = QVBoxLayout()
         self.chat_widget.setLayout(self.chat_layout)
@@ -59,7 +74,6 @@ class ChatWindow(QMainWindow):
         self.file_button.setFont(QFont("Arial", 12))
         self.file_button.clicked.connect(self.upload_file)
         self.chat_layout.addWidget(self.file_button)
-
 
         self.history_text = QTextEdit()
         self.history_text.setReadOnly(True)
@@ -104,16 +118,9 @@ class ChatWindow(QMainWindow):
         self.theme_menu_button.addMenu(self.theme_menu)
         self.chat_layout.addWidget(self.theme_menu_button)
 
-        # Aggiungi il menu delle chat a destra
-        self.central_widget.setCollapsible(0, False)
-        self.central_widget.setCollapsible(1, True)
-        self.central_widget.setSizes([600, 200])
-
-        # Aggiungi pulsante "New Chat"
-        self.new_chat_button = QPushButton("New Chat")
-        self.new_chat_button.setFont(QFont("Arial", 12))
-        self.new_chat_button.clicked.connect(self.create_new_chat)
-        self.central_widget.layout().addWidget(self.new_chat_button)
+        # Web browser basato su Chromium
+        self.web_view = QWebEngineView()
+        self.history_layout.addWidget(self.web_view)
 
     def handle_user_input(self):
         user_input = self.input_field.text().strip()
@@ -186,79 +193,102 @@ class ChatWindow(QMainWindow):
         self.chat_text.append(f"[BOT] {current_time}: {message}")
         self.chat_text.setTextColor(QColor("black"))
 
-        # Aggiungi la risposta del chatbot allo storico delle chat
+        # Aggiungi la risposta del bot allo storico delle chat
         self.chat_history.append({"timestamp": current_time, "sender": "BOT", "message": message})
 
     def upload_file(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.ReadOnly
-
-        file_path, _ = QFileDialog.getOpenFileName(
-            self,
-            "Upload File",
-            "",
-            "All Files (*);;Text Files (*.txt)",
-            options=options
-        )
-
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, "Upload File")
         if file_path:
-            # Upload file
-            self.add_user_message(f"[FILE] {file_path}")
+            self.add_user_message(f"Uploaded file: {file_path}")
 
-    def search_online(self, query):
-        credentials = None
+    def create_new_chat(self):
+        # Pulisci la finestra di chat
+        self.chat_text.clear()
 
-        if not credentials or not credentials.valid:
-            if credentials and credentials.expired and credentials.refresh_token:
-                credentials.refresh(Request())
-            else:
-                credentials = credentials.from_service_account_file(
-                    "service_account.json",
-                    target_principal="your_target_service_account@example.com"
-                )
-
-        with impersonated_credentials.Credentials(
-            credentials=credentials,
-            target_principal="your_target_service_account@example.com",
-            target_scopes=["https://www.googleapis.com/auth/cloud-platform"],
-            lifetime=3600,
-        ) as impersonated_creds:
-            search_results = list(search(query, num_results=5, credentials=impersonated_creds))
-
-        return search_results
+        # Resetta la cronologia delle chat
+        self.chat_history = []
 
     def toggle_diction(self):
         if self.diction_button.isChecked():
-            engine.setProperty("rate", 150)  # Default speed
+            self.diction_button.setText("Toggle Diction: ON")
+            engine.setProperty("volume", 1.0)
         else:
-            engine.setProperty("rate", 100)  # Reduced speed
+            self.diction_button.setText("Toggle Diction: OFF")
+            engine.setProperty("volume", 0.0)
 
     def change_theme(self, action):
-        theme = action.text()
-
-        if theme == "Light Theme":
+        if action == self.theme_action_light:
+            self.theme_action_light.setChecked(True)
+            self.theme_action_dark.setChecked(False)
             self.set_theme("light")
-        elif theme == "Dark Theme":
+        elif action == self.theme_action_dark:
+            self.theme_action_light.setChecked(False)
+            self.theme_action_dark.setChecked(True)
             self.set_theme("dark")
 
     def set_theme(self, theme):
-        palette = self.palette()
-
         if theme == "light":
-            palette.setColor(QPalette.Base, Qt.white)
-            palette.setColor(QPalette.Text, Qt.black)
+            palette = QPalette()
+            palette.setColor(QPalette.Window, QColor(255, 255, 255))
+            palette.setColor(QPalette.WindowText, QColor(0, 0, 0))
+            palette.setColor(QPalette.Base, QColor(240, 240, 240))
+            palette.setColor(QPalette.AlternateBase, QColor(220, 220, 220))
+            palette.setColor(QPalette.ToolTipBase, QColor(255, 255, 220))
+            palette.setColor(QPalette.ToolTipText, QColor(0, 0, 0))
+            palette.setColor(QPalette.Text, QColor(0, 0, 0))
+            palette.setColor(QPalette.Button, QColor(240, 240, 240))
+            palette.setColor(QPalette.ButtonText, QColor(0, 0, 0))
+            palette.setColor(QPalette.BrightText, QColor(255, 0, 0))
+            palette.setColor(QPalette.Link, QColor(0, 0, 255))
+            palette.setColor(QPalette.Highlight, QColor(0, 120, 215))
+            palette.setColor(QPalette.HighlightedText, QColor(255, 255, 255))
+            QApplication.setPalette(palette)
         elif theme == "dark":
-            palette.setColor(QPalette.Base, Qt.black)
-            palette.setColor(QPalette.Text, Qt.white)
+            palette = QPalette()
+            palette.setColor(QPalette.Window, QColor(53, 53, 53))
+            palette.setColor(QPalette.WindowText, QColor(255, 255, 255))
+            palette.setColor(QPalette.Base, QColor(25, 25, 25))
+            palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+            palette.setColor(QPalette.ToolTipBase, QColor(255, 255, 220))
+            palette.setColor(QPalette.ToolTipText, QColor(255, 255, 255))
+            palette.setColor(QPalette.Text, QColor(255, 255, 255))
+            palette.setColor(QPalette.Button, QColor(53, 53, 53))
+            palette.setColor(QPalette.ButtonText, QColor(255, 255, 255))
+            palette.setColor(QPalette.BrightText, QColor(255, 0, 0))
+            palette.setColor(QPalette.Link, QColor(42, 130, 218))
+            palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+            palette.setColor(QPalette.HighlightedText, QColor(0, 0, 0))
+            QApplication.setPalette(palette)
 
-        self.setPalette(palette)
+    def handle_search(self):
+        query = self.search_input.text().strip()
+        if query:
+            search_results = self.search_online(query)
+            self.add_bot_message(f"You asked me to search for '{query}'. Here are some results:")
 
-    def create_new_chat(self):
-        self.chat_text.clear()
-        self.history_text.clear()
-        self.chat_history = []
+            for i, result in enumerate(search_results):
+                self.add_bot_message(f"{i+1}. {result}")
 
-app = QApplication([])
-window = ChatWindow()
-window.show()
-app.exec_()
+            # Text-to-speech synthesis
+            engine.say("Here are some search results:")
+            engine.runAndWait()
+
+            for i, result in enumerate(search_results):
+                engine.say(f"Result {i+1}: {result}")
+                engine.runAndWait()
+
+        self.search_input.clear()
+
+    def search_online(self, query):
+        search_results = []
+        for result in search(query, num_results=5):
+            search_results.append(result)
+        return search_results
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = ChatWindow()
+    window.show()
+    app.exec()
